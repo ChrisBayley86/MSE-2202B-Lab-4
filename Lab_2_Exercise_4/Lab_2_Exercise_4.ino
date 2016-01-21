a0c990602cda4ff737c699eea37db603210e5b7e
codiopathy <christophern.bayley@gmail.com>
2016-01-20 21:22:31 -0500

    Switch & Button
    I added code to allow the state of both the dpdt switch and the button
    to be read by the program.

--- Lab_2_Exercise_4/Lab_2_Exercise_4.ino
+++ Lab_2_Exercise_4/Lab_2_Exercise_4.ino
@@ -10,6 +10,8 @@ void setup() {
   const int pin3 = 6;
   const int pin4 = 7;
   const int pot = A0;
+  const int dpdtSwitch = 3;
+  const int button = 2;
   
   Serial.begin(9600);
   
@@ -19,6 +21,8 @@ void setup() {
   pinMode(pin3, OUTPUT);
   pinMode(pin4, OUTPUT);
   pinMode(pot, INPUT);
+  pinMode(button, INPUT);
+  pinMode(dpdtSwitch, INPUT);
   
   //Sets all LED pins to low
   //thus starting in State 1
@@ -26,10 +30,16 @@ void setup() {
   digitalWrite(pin2, LOW);
   digitalWrite(pin3, LOW);
   digitalWrite(pin4, LOW);
+  digitalWrite(button, HIGH); 
+  //the above line enables the internal
+  //pull-up resistor.
   
 }
 
 void loop () {
+  
+  
+  
 const int ledLifeDuration = 2;
   int numPins = 4;
   int pins[numPins][5]; 
@@ -38,6 +48,9 @@ const int ledLifeDuration = 2;
   //2nd row is pin life
   //3rd row is turn-on state 1
   //4th row is turn-on state 2
+  
+  
+  
   
   //Filling the pins array
   for (int i = 0; i < numPins; i++){ //Begin for 1
@@ -54,6 +67,8 @@ const int ledLifeDuration = 2;
   //is turned back on at state 12
   
   
+  
+  
   //Declares some initial variables
   long steadyTime = millis();
   //int state = 0;
@@ -63,22 +78,33 @@ const int ledLifeDuration = 2;
   //Potentiometer pin setup
   const int pot = A0;
   pinMode(pot, INPUT);
+  const int dpdtSwitch = 3;
+  const int button = 2;
+  
+  
+  
   
   
   for (int state = 0; state <= numPins*3;){ //begin for 2
     if ((millis() - steadyTime) >= timeDifference){ //begin if 1
       
+      
+      /*
       Serial.print("State: ");
       Serial.println(state);
+      */
+      
       
       //Loop-checking of the i-th pin's state.
       for (int i = 0; i <= numPins; i++){ //begin for 4
         
         if ( (state == pins[i][3]) || (state == pins[i][4]) ){ //begin if 2
           digitalWrite(pins[i][0],HIGH);
-          Serial.println();
+          
+          /*Serial.println();
           Serial.println(state);
-          Serial.println();
+          Serial.println();*/
+          
         } // end if 2
         
         
@@ -93,12 +119,13 @@ const int ledLifeDuration = 2;
           pins[i][2] = pins[i][2]+1;
         } //end if 4
         
-       
+       /*
       Serial.print(i);
       Serial.print(": ");
       Serial.print(digitalRead(pins[i][0]));
       Serial.print(" Life: ");
       Serial.println(pins[i][2]);
+      */
         
         
     
@@ -112,8 +139,23 @@ const int ledLifeDuration = 2;
       steadyTime = millis(); 
       state++;
     } //end if 1
+   Serial.print("B: ");
+   Serial.println(digitalRead(button));
+   Serial.print("S: ");
+   Serial.println(digitalRead(dpdtSwitch));
    
-   timeDifference = analogRead(pot);
+   //Changes the method of setting the timeDifference
+   if (digitalRead(dpdtSwitch) == HIGH){ //begin if 6
+     timeDifference = analogRead(pot);
+   }
+   else if (digitalRead(dpdtSwitch) == LOW && digitalRead(button) == LOW) {
+     timeDifference += 5;//digitalRead(button);
+     
+   }
+   
+   
+   
+   
   } //end for 2
   
 }
